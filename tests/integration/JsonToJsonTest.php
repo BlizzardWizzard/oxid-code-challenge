f341de9b6bd04c6ee61e8a0cbf75e08f1af2f1c0
jstechmann <stechmann@secondred.de>
2024-05-18 17:43:17 +0200

    add 0 test to JsonToJsonTest
    Signed-off-by: jstechmann <stechmann@secondred.de>

--- tests/integration/JsonToJsonTest.php
+++ tests/integration/JsonToJsonTest.php
@@ -2,8 +2,23 @@
 
 namespace currencyConverterTests\integration;
 
+use currencyConverter\currencyConverters\JsonCurrencyConverter;
+use currencyConverter\dataSources\JsonDataSource;
 use PHPUnit\Framework\TestCase;
 
 class JsonToJsonTest extends TestCase
 {
+    /**
+     * @throws \JsonException
+     */
+    public function testJsonToJsonZeroBaseCurrency(): void
+    {
+        $dataSource = new JsonDataSource(file_get_contents('./data/testdata.json'));
+
+        $converter = new JsonCurrencyConverter($dataSource);
+
+        $convertedValues = $converter->convert(0, $dataSource->getBaseCurrency());
+
+        $this->assertSame('{"EUR":0,"USD":0,"CHF":0,"CNY":0}', $convertedValues);
+    }
 }
