f5d402f4c0e1aa4d1782a9089d23ef456efec2f1
jstechmann <stechmann@secondred.de>
2024-05-18 17:42:15 +0200

    add more test cases in JsonCurrencyConverterTest::convertProvider
    Signed-off-by: jstechmann <stechmann@secondred.de>

--- tests/unit/currencyConverters/JsonCurrencyConverterTest.php
+++ tests/unit/currencyConverters/JsonCurrencyConverterTest.php
@@ -25,9 +25,88 @@ class JsonCurrencyConverterTest extends TestCase
             [
                 0,
                 $datasource,
-                '' //TODO actually write out the expected result
+                '{"EUR":0,"USD":0,"CHF":0,"CNY":0}'
+            ],
+            [
+                1,
+                $datasource,
+                '{"EUR":1,"USD":5,"CHF":0.97,"CNY":2.3}'
+            ],
+            [
+                2,
+                $datasource,
+                '{"EUR":2,"USD":10,"CHF":1.94,"CNY":4.6}'
+            ],
+            [
+                4,
+                $datasource,
+                '{"EUR":4,"USD":20,"CHF":3.88,"CNY":9.2}'
+            ],
+            [
+                8,
+                $datasource,
+                '{"EUR":8,"USD":40,"CHF":7.76,"CNY":18.4}'
+            ],
+            [
+                16,
+                $datasource,
+                '{"EUR":16,"USD":80,"CHF":15.52,"CNY":36.8}'
+            ],
+            [
+                32,
+                $datasource,
+                '{"EUR":32,"USD":160,"CHF":31.04,"CNY":73.6}'
+            ],
+            [
+                64,
+                $datasource,
+                '{"EUR":64,"USD":320,"CHF":62.08,"CNY":147.2}'
+            ],
+            [
+                128,
+                $datasource,
+                '{"EUR":128,"USD":640,"CHF":124.16,"CNY":294.4}'
+            ],
+            [
+                256,
+                $datasource,
+                '{"EUR":256,"USD":1280,"CHF":248.32,"CNY":588.8}'
+            ],
+            [
+                512,
+                $datasource,
+                '{"EUR":512,"USD":2560,"CHF":496.64,"CNY":1177.6}'
+            ],
+            [
+                0.5,
+                $datasource,
+                '{"EUR":0.5,"USD":2.5,"CHF":0.485,"CNY":1.15}'
+            ],
+            [
+                1.0,
+                $datasource,
+                '{"EUR":1,"USD":5,"CHF":0.97,"CNY":2.3}'
+            ],
+            [
+                1.5,
+                $datasource,
+                '{"EUR":1.5,"USD":7.5,"CHF":1.455,"CNY":3.45}'
+            ],
+            [
+                2.5,
+                $datasource,
+                '{"EUR":2.5,"USD":12.5,"CHF":2.425,"CNY":5.75}'
+            ],
+            [
+                5.0,
+                $datasource,
+                '{"EUR":5,"USD":25,"CHF":4.85,"CNY":11.5}'
+            ],
+            [
+                10.0,
+                $datasource,
+                '{"EUR":10,"USD":50,"CHF":9.7,"CNY":23}'
             ]
-            //TODO add more cases
         ];
     }
 
